932ae863648ef58fb37644ed2083fcf26434849e
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-03-09 18:18:46 -0600

    Hero section

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,5 +1,5 @@
 
-   <<section id="hero" class="d-fex align-items-center">
+   <!--<<section id="hero" class="d-fex align-items-center">
         <div class="container">
             <h1> Bienvenido a <span>Orgon Diesel</span></h1>
             <h2>Repuestos al instante</h2>
@@ -7,5 +7,61 @@
                 <a href="" class="btn-action">Ver más...</a>
             </div>
         </div>
-    </section>
+    </section>-->
+
+   <div id="hero" class="carousel slide carousel-fade" data-ride="carousel">
+       <ol class="carousel-indicators">
+           <li data-target="#hero" data-slide-to="0" class="active"></li>
+           <li data-target="#hero" data-slide-to="1"></li>
+           <li data-target="#hero" data-slide-to="2"></li>
+       </ol>
+       <div class="carousel-inner">
+           <div class="carousel-item active">
+               <img src="{{ asset('./img/hero/slider0.jpg') }}" class="d-block w-100" alt="...">
+               <div class="carousel-container">
+                   <div class="hero-caption">
+                       <h1> Repuestos <span>BOSH</span></h1>
+                       <h2>Las mejores partes para tu vehiculo</h2>
+                       <div class="d-flex">
+                           <a href="" class="btn-action">Ver más...</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+           <div class="carousel-item">
+               <img src="{{ asset('./img/hero/slider1.jpg') }}" class="d-block w-100" alt="...">
+               <div class="carousel-container">
+                   <div class="hero-caption">
+                       <h1> Lubricantes <span>YUKO</span></h1>
+                       <h2>Loa mejores aceites automotrices e industriales, con los mejores estándares de calidad</h2>
+                       <div class="d-flex">
+                           <a href="" class="btn-action">Ver más...</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div class="carousel-item">
+               <img src="{{ asset('./img/hero/slider3.jpg') }}" class="d-block w-100" alt="...">
+               <div class="carousel-container">
+                   <div class="hero-caption">
+                       <h1> Bienvenido a <span>Orgon Diesel</span></h1>
+                       <h2>Repuestos al instante</h2>
+                       <div class="d-flex">
+                           <a href="" class="btn-action">Ver más...</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <a class="carousel-control-prev" href="#hero" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+       </a>
+       <a class="carousel-control-next" href="#hero" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+       </a>
+   </div>
+
 
