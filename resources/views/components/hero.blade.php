e91781f22fda4b4733bfdb53985ca1d96b612906
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-03-10 16:33:56 -0600

    adding hero section items

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -5,10 +5,26 @@
            <li data-target="#hero" data-slide-to="0" class="active"></li>
            <li data-target="#hero" data-slide-to="1"></li>
            <li data-target="#hero" data-slide-to="2"></li>
+           <li data-target="#hero" data-slide-to="3"></li>
+           <li data-target="#hero" data-slide-to="4"></li>
        </ol>
        <div class="carousel-inner">
            <div class="carousel-item active">
-               <img src="{{ asset('./img/hero/bosh.jpg') }}" class="d-block w-100" alt="...">
+               <img src="{{ asset('./img/hero/ORGON.jpg') }}" class="d-block w-100" alt="Orgon website">
+               <div class="carousel-container">
+                   <div class="hero-caption">
+                       {{--<img src="{{ asset('./img/full_orgondiesel.png') }}" alt="Orgon Diesel Logo">--}}
+                       <h1>Bienvenido a<br><span>ORGON DIESEL</span></h1>
+                       <h2>Repuestos al instante</h2>
+                       <div class="d-flex">
+                           <a href="#about" class="btn-action">Quines somos</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+           <div class="carousel-item">
+               <img src="{{ asset('./img/hero/BOSCH-2021.jpg') }}" class="d-block w-100" alt="Orgon bosch 2021">
                <div class="carousel-container">
                    <div class="hero-caption">
                        <h1> Repuestos <span>BOSCH</span></h1>
@@ -21,11 +37,11 @@
            </div>
 
            <div class="carousel-item">
-               <img src="{{ asset('./img/hero/yuko-lubricants.jpg') }}" class="d-block w-100" alt="...">
+               <img src="{{ asset('./img/hero/YUKO.jpg') }}" class="d-block w-100" alt="Orgon yuko 2021">
                <div class="carousel-container">
                    <div class="hero-caption">
                        <h1> Lubricantes <span>YUKO</span></h1>
-                       <h2>Los mejores aceites automotrices e industriales, con los mejores estándares de calidad</h2>
+                       <h2>Los mejores aceites automotrices e industriales<br><br>Calidad Europea</h2>
                        <div class="d-flex">
                            <a href="" class="btn-action">Ver más...</a>
                        </div>
@@ -33,11 +49,23 @@
                </div>
            </div>
            <div class="carousel-item">
-               <img src="{{ asset('./img/hero/atech-turbos.jpg') }}" class="d-block w-100" alt="...">
+               <img src="{{ asset('./img/hero/MCBEE.jpg') }}" class="d-block w-100" alt="Orgon mcbee">
                <div class="carousel-container">
                    <div class="hero-caption">
-                       <h1> Bienvenido a <span>Orgon Diesel</span></h1>
-                       <h2>Repuestos al instante</h2>
+                       <h1>Repuestos <span>McBee</span></h1>
+                       <h2>Los mejores repuestos para transporte pesado</h2>
+                       <div class="d-flex">
+                           <a href="" class="btn-action">Ver más...</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div class="carousel-item">
+               <img src="{{ asset('./img/hero/ATECH.jpg') }}" class="d-block w-100" alt="Orgon atech">
+               <div class="carousel-container">
+                   <div class="hero-caption">
+                       <h1>Turbos <span>ATECH</span></h1>
+                       <h2>Los mejores turbos para tu maquinaria</h2>
                        <div class="d-flex">
                            <a href="" class="btn-action">Ver más...</a>
                        </div>
